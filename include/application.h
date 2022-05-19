22294c071c297a58567709ccb88498e093252710
El_isra <tatochin-m@hotmail.com>
2022-05-19 10:55:52 -0300

    test & brief locale support

--- include/application.h
+++ include/application.h
@@ -1,6 +1,18 @@
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+	#include <wx/wx.h>
+#endif
+
+#include <wx/app.h>
 
 class MyApp : public wxApp
 {
 public:
+    MyApp() {m_lang = wxLANGUAGE_UNKNOWN;}
+    ~MyApp();
 	virtual bool OnInit();
+protected:
+	wxLanguage m_lang;  // language specified by user
+	wxLocale m_locale;  // locale we'll be using
 };
